e164dfe9b0b17e9e1e5ef0ea440075d23eafd502
Marcus Jackson <mjacks111@users.noreply.github.com>
2017-10-29 21:45:56 -0700

    CRANE ARENA
    can contract and retract crane arms, but am unable to have it pick up a game object. So for now am using a fixed joint

---
+++ CRANE/Assets/Scripts/clawClose1.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clawClose1 : MonoBehaviour {
+    
+    private bool lowerLimit,upperLimit = false;
+
+
+
+    
+
+
+    // Use this for initialization
+    void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        /*HingeJoint hinge = GetComponent<HingeJoint>();
+
+        if (Input.GetKey("q"))
+        {
+            hinge.useMotor = true;
+        }
+        */
+        
+
+
+
+       if (Input.GetKey("q") && lowerLimit == false)
+        {
+            upperLimit = false;
+            
+            transform.Rotate(0, 0, -10);
+            
+        }
+
+        if(Input.GetKey("e") && upperLimit == false )
+        {
+            lowerLimit = false;
+            transform.Rotate(0, 0, +10);
+          
+        }
+
+        if (transform.rotation.z <= -.1)
+        {
+            lowerLimit = true;
+        }
+
+        if (transform.rotation.z >= .4559173)
+        {
+            upperLimit = true;
+        }
+		
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        HingeJoint hinge = GetComponent<HingeJoint>();
+        JointMotor motor = hinge.motor;
+        if(collision.gameObject.tag == "debris")
+        {
+            Debug.Log("hit the debirs in pivot");
+            motor.force = 0;
+        }
+    }
+}
---
+++ CRANE/Assets/Scripts/sticky.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sticky : MonoBehaviour {
+    
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var joint = gameObject.AddComponent<FixedJoint>();
+        joint.connectedBody = collision.rigidbody;
+        if(Input.GetKey("e"))
+        {
+            Destroy(joint);
+        }
+    }
+}
