11e8162cb1f2d429110f4b78d0ef84a971c7db51
Jordan Humphrey <jphumphrey2@gmail.com>
2017-12-01 21:07:41 -0800

    Add files via upload

---
+++ Main_Menu/Assets/scripts/skybox_rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skybox_rotate : MonoBehaviour {
+    public float speedMultiplier;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Sets the float value of "_Rotation", adjust it by Time.time and a multiplier.
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speedMultiplier);
+    }
+}
